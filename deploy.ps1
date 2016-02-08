060b42cc113172c629cce8d72e952536db81f9d0
Phil Britton <pbritton@dacgroup.com>
2016-02-08 15:18:48 -0500

    Support New RM Commands

--- deploy.ps1
+++ deploy.ps1
@@ -4,11 +4,9 @@ $subscriptionId = "Put Your Subscription Id Here";
 $location = "West Europe"
 
 
-Add-AzureAccount
+Login-AzureRmAccount
 
-Switch-AzureMode AzureResourceManager
+Set-AzureRmContext -SubscriptionId $subscriptionId
 
-Select-AzureSubscription -SubscriptionId $subscriptionId -Current
-
-New-AzureResourceGroup -ResourceGroupName $resourceGroupName -location $location `
--TemplateFile .\azuredeploy.json -TemplateParameterFile .\azuredeploy.parameters.json
+New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
+New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile .\azuredeploy.json -TemplateParameterFile .\azuredeploy.parameters.json
